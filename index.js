0aa908b7e6fbee4ef6bcf5532d842f481ac5cb00
George Gore <georgelgore@gmail.com>
2017-12-09 16:06:50 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your code in this file!
+function scuberGreetingForFeet(ride){
+  if (ride < 400){
+    return "This one is on me!"
+  }
+  else if (ride > 2000) {
+    if (ride < 2500){return 'I will gladly take your thirty bucks.'}
+    else {return "No can do."}
+  }
+
+}
+
+
+function ternaryCheckCity(city){
+  let hey = ''
+  city == 'NYC' ? hey = 'Ok, sounds good.' : hey = 'No go.'
+  return hey
+}
+
+
+function switchOnCharmFromTip(mood){
+  switch (mood){
+  case 'generous':
+    phrase = 'Thank you so much.';
+    break;
+  case 'not as generous':
+    phrase = 'Thank you.';
+    break;
+  default:
+    phrase = "Bye.";
+    break;
+  }
+  return phrase
+}
