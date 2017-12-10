29767526be46923493dd5328e7f288ade6af50f6
psarma89 <priyam.sarma@gmail.com>
2017-12-10 11:14:14 -0500

    Done.

--- index.js
+++ index.js
@@ -3,13 +3,10 @@ function scuberGreetingForFeet(ft) {
   switch (true) {
     case ft <=400:
       return 'This one is on me!'
-      break;
     case (ft > 2000 && ft <= 2500):
       return 'I will gladly take your thirty bucks.'
-      break;
     case ft > 2500:
       return 'No can do.'
-      break;
     default:
   }
 }
@@ -22,10 +19,8 @@ function switchOnCharmFromTip(tip) {
   switch (tip) {
     case 'generous':
       return 'Thank you so much.'
-      break;
     case 'not as generous':
       return 'Thank you.'
-      break;
     default:
       return 'Bye.'
   }
