f22478ef586e42a9e8e75ce848643b40e0800f86
erickalexander <erickcamacho1@gmail.com>
2017-12-11 01:02:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(distance){
+  if (distance <= 400){
+    return 'This one is on me!'
+  }
+
+  else if (distance => 2000) {
+    if (distance < 2500){
+      return 'I will gladly take your thirty bucks.'
+    }
+    else {
+      return 'No can do.'
+    }
+  }
+}
+
+function ternaryCheckCity(city){
+  if (city === 'NYC'){
+    return 'Ok, sounds good.'
+  }
+  else {
+    return 'No go.'
+  }
+
+}
+
+function switchOnCharmFromTip(tip){
+  let responce = ''
+
+  switch (tip){
+    case 'generous':
+    responce = 'Thank you so much.'
+    break;
+    case 'not as generous':
+    responce = 'Thank you.'
+    break;
+    case 'thanks for everything':
+    responce = 'Bye.'
+    break;
+  }
+
+  return responce
+}
