6c1b302a0e3f5bcadf719e63836c54e73abcae16
Josh-Stillman <joshstillman@gmail.com>
2017-12-12 17:14:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your code in this file!
+function scuberGreetingForFeet(input){
+  if (input < 200) {
+    return  'This one is on me!'
+  } else if ( input > 2000 && input <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (input > 2500) {
+    return 'No can do.'
+  }
+
+}
+
+function ternaryCheckCity(city){
+  switch (city){
+    case 'NYC':
+      return 'Ok, sounds good.'
+      break
+    default:
+        return 'No go.'
+      break
+  }
+}
+
+function switchOnCharmFromTip(input){
+  switch (input){
+    case 'generous':
+      return 'Thank you so much.'
+      break
+    case 'not as generous':
+      return 'Thank you.'
+      break
+    default:
+      return 'Bye.'
+      break
+  }
+}
